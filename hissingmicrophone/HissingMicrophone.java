4c16872271144cd35ae7a86c8d3b2c76cb12cd25
Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>
2020-11-03 11:09:16 +0700

    add hissing microphone

--- hissingmicrophone/HissingMicrophone.java
+++ hissingmicrophone/HissingMicrophone.java
@@ -5,14 +5,6 @@ public class HissingMicrophone {
      public static void main(String []args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String word = reader.readLine();
-        HashSet<Character> hset = new HashSet<Character>();
-        for(int i=0;i<word.length();i++) {
-            hset.add(word.charAt(i));
-        }
-        if (hset.size() != word.length()-1) {
-            System.out.println("hiss");
-        } else {
-            System.out.println("no hiss");
-        }
+        System.out.println(word.contains("ss") ? "hiss" : "no hiss");
      }
 }
